99d47e4844f396b0e28eff382c800559f374ce37
avyaanverma <avyaanver@gmail.com>
2026-06-30 20:40:31 +0530

    done

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,8 +1,11 @@
 import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home";
 import EditorPage from "../features/editor/pages/EditorPage";
 import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router";
 
+const router = createBrowserRouter([
 const router = createBrowserRouter([
     {
         path: "/",
@@ -13,6 +16,12 @@ const router = createBrowserRouter([
         element: <EditorPage />,
     },
 ]);
+
+const AppRoutes = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
 
 const AppRoutes = () => {
   return (
