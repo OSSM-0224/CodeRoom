7fe2503d46d89bb84dba7f2c140e6345ffa951b2
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-30 21:11:56 +0530

    testing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,17 @@
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './index.css';
-import { store } from './app/store.js';
-import AppRoutes from './routes/AppRoutes.jsx';
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+import { store } from "./app/store.js";
+import AppRoutes from "./routes/AppRoutes.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <RouterProvider router={AppRoutes} />
+    console.log("API URL:", import.meta.env.VITE_API_URL); console.log("Socket
+    URL:", import.meta.env.VITE_SOCKET_URL);
     <ToastContainer />
-  </Provider>
+  </Provider>,
 );
