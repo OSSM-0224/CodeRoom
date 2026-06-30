38fa1e90083e957c48cedef94548994b31af1239
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-30 19:40:12 +0530

    added the useEditor hook

--- client/src/features/editor/hooks/useEditor.js
+++ client/src/features/editor/hooks/useEditor.js
@@ -1,67 +1,32 @@
 import { axiosInstace } from "../../../services/axios";
 
-import editorApi from "../api/editorApi.js";
-
-
-
-export const getDocument = async (roomId, setCode) => {
-
-    try {
-
-        const response = await axiosInstace.get(`/document/${roomId}`);
-
-
-        setCode(response.data.data.content);
-
-    } catch (error) {
-
-        console.log(error);
-
-    }
-
-
+export const getDocument = async (roomId) => {
+  const { data } = await axiosInstace.get(/document/);
+  return data?.data ?? null;
 };
 
-
 export const saveDocument = async (roomId, code, language) => {
-
-    try {
-
-        await editorApi.updateDocument(roomId, {
-            content: code,
-            language,
-        });
-
-        console.log("Saved");
-
-    } catch (error) {
-
-        console.log(error);
-
-    }
-
+  const { data } = await axiosInstace.patch(/document/, {
+    content: code,
+    language,
+  });
+  return data?.data ?? null;
 };
 
 export const getParticipants = async (roomId) => {
-
-    const { data } = await axiosInstace.get(`/room/${roomId}`);
-
-    return data.data;
+  const { data } = await axiosInstace.get(/room/);
+  return data?.data ?? [];
 };
 
 export const getRoomData = async (roomId) => {
-
-    const { data } = await axiosInstace.get(`/room/user/${roomId}`);
-
-    return data.data;
+  const { data } = await axiosInstace.get(/room/user/);
+  return data?.data ?? null;
 };
 
-
 export const leaveRoomApi = async (username) => {
+  const resp = await axiosInstace.post("/room/leave", {
+    username,
+  });
 
-    const resp = await axiosInstace.post("/room/leave", {
-        username,
-    });
-
-    return resp.data;
+  return resp.data;
 };
