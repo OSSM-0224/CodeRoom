aca40caaa21e8e5dcf56ef12a7d5d227010a3824
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-30 13:57:29 +0530

    added the resgisterEditorSocketHandlers in the socket index

--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,8 +1,10 @@
 import { io } from "../server.js";
+import registerEditorSocketHandlers from "./editor.socket.js";
 
 const initializeSocket = () => {
   io.on("connection", (socket) => {
     console.log("🟢 Connected:", socket.id);
+    registerEditorSocketHandlers(io, socket);
     socket.on("disconnect", () => {
       console.log("🔴 Disconnected:", socket.id);
     });
